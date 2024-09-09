dba8df136794e02f16934a6c23847c96044709a5
samsonkaleti <kaletishyam@gmail.com>
2024-09-09 23:50:35 +0530

    creating server

--- 2.NodejsServer/serverNode.js
+++ 2.NodejsServer/serverNode.js
@@ -10,4 +10,5 @@ const createServer = server.createServer(req, res, () => {
 
 createServer.listen(8083, () => {
     console.log('Server is running on port 8001')
+    res.writeHead(200, {'Content-Type': 'text/plain'});
 })
